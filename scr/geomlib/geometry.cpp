accbeed6c3b427130536f6574a3193fa12da9f63
Sasha Mizin <mizin.alexx@gmail.com>
2020-10-15 15:54:51 +0300

    added compDeterminant3D

--- scr/geomlib/geometry.cpp
+++ scr/geomlib/geometry.cpp
@@ -1,1 +1,22 @@
 #include "geometry.h"
+
+Vector3D::Vector3D(const double x, const double y, const double z) :
+    x_(x), y_(y), z_(z) {}
+
+Vector3D Vector3D::operator-(const Vector3D& lhs) const {
+    return Vector3D(x_ - lhs.x_, y_ - lhs.y_, z_ - lhs.z_);
+}
+
+Segment3D::Segment3D(const Vector3D start, const Vector3D end) :
+    start_(start), end_(end) {}
+
+double compDeterminant3D(const Vector3D& v1, const Vector3D& v2,
+                         const Vector3D& v3)
+{
+    return  v1.x_ * v2.y_ * v3.z_ +
+            v2.x_ * v3.y_ * v1.z_ +
+            v3.x_ * v1.y_ * v2.z_ -
+            v3.x_ * v2.y_ * v1.z_ -
+            v2.x_ * v1.y_ * v3.z_ -
+            v1.x_ * v3.y_ * v2.z_;
+}
--- scr/geomlib/geometry.h
+++ scr/geomlib/geometry.h
@@ -1,11 +1,26 @@
 
 class Vector3D {
-    double x;
-    double y;
-    double z;
+public:
+    Vector3D(double x, double y, double z);
+    Vector3D operator-(const Vector3D& lhs) const;
+private:
+    friend  double compDeterminant3D(const Vector3D& vector1,
+                                     const Vector3D& vector2,
+                                     const Vector3D& vector3);
+
+    double x_;
+    double y_;
+    double z_;
 };
 
 class Segment3D {
-    Vector3D start;
-    Vector3D end;
+public:
+    Segment3D(Vector3D start, Vector3D end);
+
+private:
+    Vector3D start_;
+    Vector3D end_;
 };
+
+double compDeterminant3D(const Vector3D& vector1, const Vector3D& vector2,
+                         const Vector3D& vector3);
